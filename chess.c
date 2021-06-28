bc9f51c645a938fad2385b634981f53008d5c3d2
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-28 15:11:11 -0400

    Added Board Initialisation

--- chess.c
+++ chess.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main () {
+	printf("Welcome to Terminal Chess!\n");
+	printf("Initializing Board...")	
+	int [8][8] = { {5, 4, 3, 1, 2, 3, 4, 5}
+				 , {6, 6, 6, 6, 6, 6, 6, 6}
+				 , {0, 0, 0, 0, 0, 0, 0, 0}
+				 , {0, 0, 0, 0, 0, 0, 0, 0}
+				 , {0, 0, 0, 0, 0, 0, 0, 0}
+				 , {0, 0, 0, 0, 0, 0, 0, 0} 
+				 , {0, 0, 0, 0, 0, 0, 0, 0}
+				 , {6, 6, 6, 6, 6, 6, 6, 6}
+				 , {5, 4, 3, 1, 2, 3, 4, 5}
+				 }
+	printf("Board Initialized!")
+	
+	// rest of game...
+	
+	printf("Terminating...")
+}
