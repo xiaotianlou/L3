6e0a1d580130368d41929d4d718433b4c3dc8d32
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-28 16:25:37 -0400

    Started Valid move logic

--- chess.c
+++ chess.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool validMove (char* input) {
-	// Add move validity checks some other time! Too lazy!! 
-	return True;
+bool memberOf (int *ValidMoves[2], int x, int y) ; 
+
+bool validMove (char* input, int* board[8][8]) {
+	int start_x = input[0] - 48;
+	int start_y = input[1] - 48;
+	int end_x = input[3] - 48;
+	int end_y = input[4] - 48;
+	
+	// out of bounds check
+	if (start_x > 7 || start_x < 0) {
+		return false;
+	} else if start_y > 7 || start_y < 0) {
+		return false;
+	} else if end_x > 7 || end_x < 0) {
+		return false;
+	} else if end_y > 7 || end_y < 0) {
+		return false;
+	}
+	
+	// piece move checks 
+
+	switch (*board[start_x][start_y]) {
+		case 0: // moving a blank spot is never valid
+			return false;
+		case 1: // King
+			break;
+		case 2: // Queen
+			break;
+		case 3 : // Bishop
+			break;
+		case 4 : // Knight
+			break;
+		case 5 : // Rook
+			break;
+		case 6 : // Pawn
+			break;
+	}
+	
+	if (!memberOf(validMoves, end_x, end_y)) {
+		return false;
+	}
+	
+	return true;
 }
 
 void makeMove (char* input, int* board[8][8]) {
