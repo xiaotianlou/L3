35c618811d0750e555c26383c6602e76fa0f4ff9
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-28 15:52:32 -0400

    Initial Turn logic

--- chess.c
+++ chess.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main () {
+	printf("Welcome to Terminal Chess!\n");
+	printf("Initializing Board...\n")	;
+	int board [8][8] = { {5, 4, 3, 1, 2, 3, 4, 5}
+				             , {6, 6, 6, 6, 6, 6, 6, 6}
+				 	           , {0, 0, 0, 0, 0, 0, 0, 0}
+					           , {0, 0, 0, 0, 0, 0, 0, 0}
+				             , {0, 0, 0, 0, 0, 0, 0, 0} 
+				             , {0, 0, 0, 0, 0, 0, 0, 0}
+				             , {6, 6, 6, 6, 6, 6, 6, 6}
+				             , {5, 4, 3, 1, 2, 3, 4, 5}
+				             } ;
+  char buf[100];
+  bool whitesMove = True;
+	printf("Board Initialized!\n") ;
+	
+	showBoard(board);
+
+  printf("Enter `q` to quit at any time.\n");
+  printf("Moves are entered as co-ordinate pairs, such as \"13-33\" \n");
+  printf("White Player to move.\n");
+  prinf("Please enter horizontal coordinate of piece to move >> ");
+  scanf("%s", buf);
+  do {
+    if (whitesMove) {
+      printf("White to move.\n");
+    } else {
+      printf("Black to move.\n");
+    }
+    printf("♔ >> ")
+    scanf("%s", buf);
+  } while (buf[0] != `q` && buf[0] != `Q`) 
+    
+	printf("Terminating...\n") ;
+}
