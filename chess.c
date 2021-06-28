157d61aec73e12f873276e6bc730b942296cf745
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-28 15:25:52 -0400

    Changed to If Statements

--- chess.c
+++ chess.c
@@ -1,46 +1,49 @@
-#inlcude<stdio.h>
+#include <stdio.h>
+
 
 // ♔ ♕ ♖ ♗ ♘ ♙
-void showBoard (int ** board) {
+void showBoard (int board[8][9]) {
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
-			switch (board[i][j]) {
-				case 0: 
-					printf(" ");
-				case 1:
-					printf("♔");
-				case 2: 
-					printf("♕");
-				case 3: 
-					printf("♗");
-				case 4: 
-					printf("♘");
-				case 5: 
-					printf("♙");
-			}
+			if (board[i][j] == 0) {
+				printf(" ");
+			} else if (board[i][j] == 1) {
+				printf("♔");
+ 			} else if (board[i][j] == 2) {
+				printf("♕");
+			} else if (board[i][j] == 3) {
+				printf("♖");
+			} else if (board[i][j] == 4) {
+				printf("♗");
+			} else if (board[i][j] == 5) {
+				printf("♘");
+			} else if (board[i][j] == 6) {
+				printf("♙");
 		}
-		print("\n")
+		printf("\n");
 	}
 }
 
 int main () {
 	printf("Welcome to Terminal Chess!\n");
-	printf("Initializing Board...")	
-	int [8][8] board = { {5, 4, 3, 1, 2, 3, 4, 5}
+	printf("Initializing Board...")	;
+	int board [8][9]  = { {5, 4, 3, 1, 2, 3, 4, 5}
 				       , {6, 6, 6, 6, 6, 6, 6, 6}
-		 		 	     , {0, 0, 0, 0, 0, 0, 0, 0}
-					     , {0, 0, 0, 0, 0, 0, 0, 0}
-				 	     , {0, 0, 0, 0, 0, 0, 0, 0}
+				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
+					   , {0, 0, 0, 0, 0, 0, 0, 0}
+				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
 				       , {0, 0, 0, 0, 0, 0, 0, 0} 
 				       , {0, 0, 0, 0, 0, 0, 0, 0}
 				       , {6, 6, 6, 6, 6, 6, 6, 6}
 				       , {5, 4, 3, 1, 2, 3, 4, 5}
-				       }
-	printf("Board Initialized!")
+				       } ;
+	printf("Board Initialized!") ;
 	
 	showBoard(board);
 	
 	// rest of game...
 	
-	printf("Terminating...")
+	printf("Terminating...") ;
+	
 }
+
