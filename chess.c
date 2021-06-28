55f898b259780efcce5ef523a8cf0c161184c3a4
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-28 15:27:32 -0400

    Added Coordinate indicators

--- chess.c
+++ chess.c
@@ -2,8 +2,10 @@
 
 
 // ♔ ♕ ♖ ♗ ♘ ♙
-void showBoard (int board[8][9]) {
+void showBoard (int board[8][8]) {
+	printf("\n\n  01234567\n");
 	for (int i = 0; i < 8; i++) {
+		printf("%d ", i); 
 		for (int j = 0; j < 8; j++) {
 			if (board[i][j] == 0) {
 				printf(" ");
@@ -19,6 +21,7 @@ void showBoard (int board[8][9]) {
 				printf("♘");
 			} else if (board[i][j] == 6) {
 				printf("♙");
+			}
 		}
 		printf("\n");
 	}
@@ -27,23 +30,22 @@ void showBoard (int board[8][9]) {
 int main () {
 	printf("Welcome to Terminal Chess!\n");
 	printf("Initializing Board...")	;
-	int board [8][9]  = { {5, 4, 3, 1, 2, 3, 4, 5}
-				       , {6, 6, 6, 6, 6, 6, 6, 6}
-				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
-					   , {0, 0, 0, 0, 0, 0, 0, 0}
-				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
-				       , {0, 0, 0, 0, 0, 0, 0, 0} 
-				       , {0, 0, 0, 0, 0, 0, 0, 0}
-				       , {6, 6, 6, 6, 6, 6, 6, 6}
-				       , {5, 4, 3, 1, 2, 3, 4, 5}
-				       } ;
+	int board [8][8]  = { {5, 4, 3, 1, 2, 3, 4, 5}
+ 				        , {6, 6, 6, 6, 6, 6, 6, 6}
+ 				 	    , {0, 0, 0, 0, 0, 0, 0, 0}
+ 					    , {0, 0, 0, 0, 0, 0, 0, 0}
+ 				        , {0, 0, 0, 0, 0, 0, 0, 0} 
+ 				        , {0, 0, 0, 0, 0, 0, 0, 0}
+ 				        , {6, 6, 6, 6, 6, 6, 6, 6}
+				        , {5, 4, 3, 1, 2, 3, 4, 5}
+				        } ;
 	printf("Board Initialized!") ;
 	
 	showBoard(board);
 	
 	// rest of game...
 	
-	printf("Terminating...") ;
+	printf("Terminating...");
 	
 }
 
