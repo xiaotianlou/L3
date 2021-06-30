cb7ebd19f97c3fa806c38757d1da24b3ec2aa704
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-30 10:51:24 -0400

    updated queen logic

--- chess.c
+++ chess.c
@@ -4,10 +4,10 @@
 //bool memberOf (int *ValidMoves[2], int x, int y) ; 
 
 bool isValidMove (char* input, int board[8][8]) {
-	int start_x = input[0] - 48;
-	int start_y = input[1] - 48;
-	int end_x = input[3] - 48;
-	int end_y = input[4] - 48;
+	int start_y = input[0] - 48;
+	int start_x = input[1] - 48;
+	int end_y = input[3] - 48;
+	int end_x = input[4] - 48;
 	
 	// out of bounds check
 	if (start_x > 7 || start_x < 0) {
@@ -31,10 +31,11 @@ bool isValidMove (char* input, int board[8][8]) {
 		case 1: // King
 			break;
 		case 2: // Queen
+		//printf("Queen Selected\n");
 			while (true) {
 				xcheck += (start_x > end_x)? -1 : 1;
 				ycheck += (start_y > end_y)? -1 : 1;
-		 		if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
+				if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
 					break;
 				}
 				if (board[xcheck][ycheck] != 0) {
@@ -47,26 +48,36 @@ bool isValidMove (char* input, int board[8][8]) {
 					return true;
 				}
 			}
+			xcheck = start_x;
+			ycheck = start_y;
+			piecesInWay = 0;		
 			while (true) {
+				//printf("Calculating Rook Movement at %d, %d\n", ycheck, xcheck);
 				if (start_y == end_y) {
+					//printf("Horzontal!\n");
 					xcheck += (start_x > end_x)? -1 : 1;
 				} else if (start_x == end_x) {
+					//printf("Vertical!\n");
 					ycheck += (start_y > end_y)? -1 : 1;
 				} 
+				//printf("Now checking %d, %d\n", ycheck, xcheck);
 			
-		 		if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
+				if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
+					//printf("Out of bounds!\n");
 					break;
 				}
 				if (board[xcheck][ycheck] != 0) {
+					//printf("Hit Piece: %d\n", board[xcheck][ycheck]);
 					piecesInWay += 1;
-				} 
+				} 	
 				if (piecesInWay == 2) {
+					//printf("Hit Second Piece: %d\n", board[xcheck][ycheck]);
 					break;
-				}
+				}	
 				if (xcheck == end_x && ycheck == end_y) {
 					return true;
 				}
-			}
+			}	
 			break;
 		case 3 : // Bishop
 			break;
