d22fda1e5cc0790e09031d9dcc4be1528ea721c3
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-29 20:14:32 -0400

    more bug fixes

--- chess.c
+++ chess.c
@@ -3,7 +3,7 @@
 int main () {
 	printf("Welcome to Terminal Chess!\n");
 	printf("Initializing Board...")	;
-	int [8][8] = { {5, 4, 3, 1, 2, 3, 4, 5}
+	int board[8][8] = { {5, 4, 3, 1, 2, 3, 4, 5}
 				 , {6, 6, 6, 6, 6, 6, 6, 6}
 				 , {0, 0, 0, 0, 0, 0, 0, 0}
 				 , {0, 0, 0, 0, 0, 0, 0, 0}
