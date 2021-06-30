9b7753e7c3254bc174743c065dceeb38e8981e9e
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-29 20:22:58 -0400

    bug fixes

--- chess.c
+++ chess.c
@@ -3,16 +3,16 @@
 
 void showBoard(int board[8][8]);
 
-bool validMove (char* input) {
+bool isValidMove (char* input, int board[8][8]) {
 	// Add move validity checks some other time! Too lazy!! 
-	return True;
+	return true;
 }
 
 void makeMove (char* input, int* board[8][8]) {
-	start_x = input[0] - 48;
-	start_y = input[1] - 48;
-	end_x = input[3] - 48;
-	end_y = input[4] - 48;
+	int start_x = input[0] - 48;
+	int start_y = input[1] - 48;
+	int end_x = input[3] - 48;
+	int end_y = input[4] - 48;
 	
 	*board[end_x][end_y] = *board[start_x][start_y];
 }
@@ -42,7 +42,7 @@ int main () {
     showBoard(board);
     do {
 	if (flag) {
-		printf("Move Invalid! Try again!")
+		printf("Move Invalid! Try again!");
 	}
     	if (whitesMove) {
       		printf("White to move.\n");
