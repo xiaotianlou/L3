0c0063f7d7d7de0f463b2773fd0627b2662e2e24
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2021-06-30 10:27:39 -0400

    swapped king and queen

--- chess.c
+++ chess.c
@@ -52,14 +52,14 @@ void showBoard (int board[8][8]) {
 int main () {
 	printf("Welcome to Terminal Chess!\n");
 	printf("Initializing Board...")	;
-	int board [8][8] = { {5, 4, 3, 1, 2, 3, 4, 5}
+	int board [8][8] = { {5, 4, 3, 2, 1, 3, 4, 5}
 				       , {6, 6, 6, 6, 6, 6, 6, 6}
 				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
 					   , {0, 0, 0, 0, 0, 0, 0, 0}
 				       , {0, 0, 0, 0, 0, 0, 0, 0} 
 				       , {0, 0, 0, 0, 0, 0, 0, 0}
 				       , {6, 6, 6, 6, 6, 6, 6, 6}
-				       , {5, 4, 3, 1, 2, 3, 4, 5}
+				       , {5, 4, 3, 2, 1, 3, 4, 5}
 				       } ;
 	printf("Board Initialized!") ;
 	
