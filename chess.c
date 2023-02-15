d7bc3a8f5d4e49c098ffa5afea880a21513831ea
xiaotian Lou <loux8@mcmaster.ca>
2023-02-15 13:10:06 -0500

    fix bug

--- chess.c
+++ chess.c
@@ -1,6 +1,97 @@
 
 
+bool isValidMove (char* input, int board[8][8]) {
+    int start_y = input[0] - 48;
+    int start_x = input[1] - 48;
+    int end_y = input[3] - 48;
+    int end_x = input[4] - 48;
 
+    // out of bounds check
+    if (start_x > 7 || start_x < 0) {
+        return false;
+    } else if (start_y > 7 || start_y < 0) {
+        return false;
+    } else if (end_x > 7 || end_x < 0) {
+        return false;
+    } else if (end_y > 7 || end_y < 0) {
+        return false;
+    }
+
+    // piece move checks
+    int* validMoves[2];
+    int xcheck = start_x;
+    int ycheck = start_y;
+    int piecesInWay = 0;
+    switch (board[start_x][start_y]) {
+        case 0: // moving a blank spot is never valid
+            return false;
+        case 1: // King
+            break;
+        case 2: // Queen
+            //printf("Queen Selected\n");
+            while (true) {
+                xcheck += (start_x > end_x)? -1 : 1;
+                ycheck += (start_y > end_y)? -1 : 1;
+                if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
+                    break;
+                }
+                if (board[xcheck][ycheck] != 0) {
+                    piecesInWay += 1;
+                }
+                if (piecesInWay == 2) {
+                    break;
+                }
+                if (xcheck == end_x && ycheck == end_y) {
+                    return true;
+                }
+            }
+            xcheck = start_x;
+            ycheck = start_y;
+            piecesInWay = 0;
+            while (true) {
+                //printf("Calculating Rook Movement at %d, %d\n", ycheck, xcheck);
+                if (start_y == end_y) {
+                    //printf("Horzontal!\n");
+                    xcheck += (start_x > end_x)? -1 : 1;
+                } else if (start_x == end_x) {
+                    //printf("Vertical!\n");
+                    ycheck += (start_y > end_y)? -1 : 1;
+                }
+                //printf("Now checking %d, %d\n", ycheck, xcheck);
+
+                if (xcheck < 0 || ycheck < 0 || xcheck > 7 || ycheck > 7) {
+                    //printf("Out of bounds!\n");
+                    break;
+                }
+                if (board[xcheck][ycheck] != 0) {
+                    //printf("Hit Piece: %d\n", board[xcheck][ycheck]);
+                    piecesInWay += 1;
+                }
+                if (piecesInWay == 2) {
+                    //printf("Hit Second Piece: %d\n", board[xcheck][ycheck]);
+                    break;
+                }
+                if (xcheck == end_x && ycheck == end_y) {
+                    return true;
+                }
+            }
+            break;
+        case 3 : // Bishop
+            break;
+        case 4 : // Knight
+            break;
+        case 5 : // Rook
+            break;
+        case 6 : // Pawn
+            break;
+    }
+    /*
+    if (!memberOf(validMoves, end_x, end_y)) {
+        return false;
+    }
+    */
+    return true;
+}
 void makeMove (char* input, int board[8][8]) {
 	int start_y = input[0] - 48;
 	int start_x = input[1] - 48;
